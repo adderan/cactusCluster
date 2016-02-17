acbed7e2276efff16ae242dabbec310b50ec42ba
Alden <adderan@ucsc.edu>
2016-02-17 14:34:20 -0800

    Add progressiveCactus to each node, since toil needs to be installed in a virtualenv in the same path on each node.
    Remove mesos-slave from the master node - not necessary anymore with grow-cluster command in cgcloud

--- src/cgcloud/cactusCluster/cactus_box.py
+++ src/cgcloud/cactusCluster/cactus_box.py
@@ -1,45 +1,27 @@
-from cgcloud.toil.toil_box import ToilBox
-from cgcloud.mesos.mesos_box import work_dir, mesos_service, user
+from cgcloud.toil.toil_box import ToilLatestBox
 from cgcloud.core.cluster import ClusterLeader, ClusterWorker
-from cgcloud.fabric.operations import sudo
+from cgcloud.fabric.operations import sudo, run
 from cgcloud.core.box import fabric_task
-from cgcloud.lib.util import heredoc
 
-class CactusBox( ToilBox ):
+class CactusBox( ToilLatestBox ):
     def _list_packages_to_install( self ):
-        return super( CactusBox, self)._list_packages_to_install() + ['git', 'python-dev']
+        return super( CactusBox, self)._list_packages_to_install() + ['git', 'g++', 'python-dev']
 
     @fabric_task
     def __fix_python( self ):
-        sudo( 'ln -fs /usr/lib/python2.7/plat-x86_64-linux-gnu/_sysconfigdata.py /usr/lib/python2.7/' )
+        sudo( 'ln -s /usr/lib/python2.7/plat-*/_sysconfigdata_nd.py /usr/lib/python2.7/' )
+
+    @fabric_task
+    def __install_cactus( self ):
+        run( 'git clone http://github.com/adderan/progressiveCactus/' )
+        run( 'cd progressiveCactus && git checkout toil' )
+        run( 'cd progressiveCactus && git submodule update --init' )
+        run( 'cd progressiveCactus && make clusterNode' )
+        
     def _post_install_packages( self ):
         super( CactusBox, self)._post_install_packages()
         self.__fix_python()
-    @fabric_task
-    def __start_mesos_node_on_master( self ):
-        service = mesos_service( 'slave',
-                           '--master=mesos-master:5050',
-                           '--no-switch_user',
-                           '--work_dir=' + work_dir,
-                           '$(cat /var/lib/mesos/slave_args)' )
-        start_on = "mesosbox-start-master"
-        self._register_init_script(
-                    service.init_name,
-                    heredoc( """
-                        description "{service.description}"
-                        console log
-                        start on {start_on}
-                        stop on runlevel [016]
-                        respawn
-                        umask 022
-                        limit nofile 8000 8192
-                        setuid {user}
-                        setgid {user}
-                        env USER={user}
-                        exec {service.command}""" ) )
-    def _post_install_mesos( self ):
-        self.__start_mesos_node_on_master()
-        super( CactusBox, self)._post_install_mesos()
+        self.__install_cactus()
 
 class CactusLeader( CactusBox, ClusterLeader):
     pass
