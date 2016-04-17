0fb9224fbda3edaf330845c150a220aa0cd1baab
Alden <adderan@ucsc.edu>
2016-04-16 18:30:10 -0700

    clone progressiveCactus into shared dir with correct permissions

--- src/cgcloud/cactusCluster/cactus_box.py
+++ src/cgcloud/cactusCluster/cactus_box.py
@@ -4,6 +4,7 @@ from cgcloud.fabric.operations import sudo, run
 from cgcloud.core.box import fabric_task
 from cgcloud.lib.util import heredoc
 from cgcloud.mesos.mesos_box import work_dir, mesos_service, user
+import os
 
 class CactusBox( ToilLatestBox ):
     def _list_packages_to_install( self ):
@@ -13,12 +14,13 @@ class CactusBox( ToilLatestBox ):
     def __fix_python( self ):
         sudo( 'ln -s /usr/lib/python2.7/plat-*/_sysconfigdata_nd.py /usr/lib/python2.7/' )
 
-    @fabric_task
+    @fabric_task ( user="mesosbox" )
     def __install_cactus( self ):
-        run( 'git clone http://github.com/adderan/progressiveCactus/' )
-        run( 'cd progressiveCactus && git checkout toil' )
-        run( 'cd progressiveCactus && git submodule update --init' )
-        run( 'cd progressiveCactus && make clusterNode' )
+        progressiveCactusDir = os.path.join(self._shared_dir(), "progressiveCactus")
+        run( 'git clone http://github.com/adderan/progressiveCactus %s' % progressiveCactusDir )
+        run( 'cd %s && git checkout toil' % progressiveCactusDir )
+        run( 'cd %s && git submodule update --init' % progressiveCactusDir)
+        run( 'cd %s && make clusterNode' % progressiveCactusDir)
 
     @fabric_task
     def __start_mesos_node_on_master( self ):
@@ -42,6 +44,9 @@ class CactusBox( ToilLatestBox ):
                         setgid {user}
                         env USER={user}
                         exec {service.command}""" ) )
+
+    def _toil_pip_args( self ):
+        return [ '--pre', 'toil[aws,mesos,encryption]' ]
         
     def _post_install_packages( self ):
         super( CactusBox, self)._post_install_packages()
